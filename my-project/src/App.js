68f2b6b9d3af5b934e9867062f04800f2a47f033
kanadsen <kanadsen01@gmail.com>
2022-09-21 22:02:17 +0530

    Upload react files

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Pathfinding_2D from './Pathfinding_2D/Pathfinding_2D';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <Pathfinding_2D></Pathfinding_2D>
     </div>
   );
 }
---
+++ my-project/src/Data select/selectdata.ts
@@ -0,0 +1,18 @@
+import { Options } from '../types/options'
+
+const selectOptions: Options[] = [
+	{
+		name: 'Dijkstra`s algorithm',
+		value: 'dijkstra',
+	},
+	{
+		name: 'A*(A Star) algorithm',
+		value: 'astar',
+	},
+	{
+		name: 'Greedy Best First Algorithm',
+		value: 'greedy',
+	},
+]
+
+export default selectOptions
